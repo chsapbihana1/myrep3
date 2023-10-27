cb3a48e2404d6f13037e6a6febf3af0f49b9e01a
chetan [ <chaitan.98@gmail.com>
2023-10-27 18:02:39 +0000

    df

--- models/example/staging/SuperStores/EMP/JOIN_Materialize_view.sql
+++ models/example/staging/SuperStores/EMP/JOIN_Materialize_view.sql
@@ -2,8 +2,7 @@
 with 
 cte3 as (select * From {{ref("EMP")}} ),
 cte4 as (select * From {{ref("DEPT")}})
-select dname,  sum(sal) From cte3 ,  cte4   
-where cte3.DEPTNO = cte4.DDEPTNO
+select dname, SUM(sal) From cte3 inner join  cte4 on 
+cte3.DEPTNO = cte4.DDEPTNO
 group by dname
- 
  
